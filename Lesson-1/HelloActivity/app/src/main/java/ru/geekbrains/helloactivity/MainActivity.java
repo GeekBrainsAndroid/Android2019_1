5358f56a972cdd3225708daee879d45257e5aaf7
DwarfPorter <mrvlmor@gmail.com>
2019-06-02 15:18:25 +0300

    HelloActivity: Обработка ресурсов в коде

--- Lesson-1/HelloActivity/app/src/main/java/ru/geekbrains/helloactivity/MainActivity.java
+++ Lesson-1/HelloActivity/app/src/main/java/ru/geekbrains/helloactivity/MainActivity.java
@@ -2,6 +2,7 @@ package ru.geekbrains.helloactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView greeting = findViewById(R.id.greeting);
+        greeting.setText(R.string.hello_geekbrains);
     }
 }
