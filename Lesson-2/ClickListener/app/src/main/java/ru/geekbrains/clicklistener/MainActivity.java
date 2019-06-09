01d8192c387f6af9acd9f0cf128df5e7b171a758
DwarfPorter <mrvlmor@gmail.com>
2019-06-09 21:53:31 +0300

    ClickListener: Обработка нажатий через анонимный класс слушатель.

--- Lesson-2/ClickListener/app/src/main/java/ru/geekbrains/clicklistener/MainActivity.java
+++ Lesson-2/ClickListener/app/src/main/java/ru/geekbrains/clicklistener/MainActivity.java
@@ -3,6 +3,7 @@ package ru.geekbrains.clicklistener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "button2 ClickListener event!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void button1_onClick(View view) {
