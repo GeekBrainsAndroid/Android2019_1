d9d2f57caec416e806e9d76abc7d946d460d0e0e
DwarfPorter <mrvlmor@gmail.com>
2019-06-23 18:57:52 +0300

    Login: Установка темы активити программно

--- Lesson-7/Login/app/src/main/java/ru/geekbrains/login/MainActivity.java
+++ Lesson-7/Login/app/src/main/java/ru/geekbrains/login/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
 
         login = findViewById(R.id.inputLoginName);
